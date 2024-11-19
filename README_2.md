fe9eae29b2593baa21d673a5a3bb2b55a2240a7d
DaneVN <Dane.vN23@gmail.com>
2024-11-19 15:31:47 +0200

    React-App npm installed + clean up

---
+++ README_2.md
@@ -0,0 +1,3 @@
+# Dependencies
+
+use `npm install` to get all tha dependecies you need installed on your local machine
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1>Very awesome project here</h1>
+    </>
+  );
+}
+
+export default App;

97bfaa5bc564cf740f05a1db1a03c2d63405f6e5
DaneVN <Dane.vN23@gmail.com>
2024-11-24 13:34:21 +0200

    LoadMore btn added + CSS

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import LastListened from "../components/LastListened";
 import PreviewCard from "../components/PreviewCard";
 import { Link } from "react-router-dom";
+import loadMore from "../images/load-more.png";
 
 export default function Home() {
   return (
@@ -10,13 +11,17 @@ export default function Home() {
       <h2>You were listening to:</h2>
       <LastListened />
       <br />
-      <div id="home-podcast-list" className="bg-lime-950 p-3">
+      <div id="home-podcast-list" className="bg-lime-950 p-3 relative">
         <Link to="show">
           <PreviewCard />
         </Link>
         {/* //have the different cards path to the corresponding show using urlParams */}
         <PreviewCard />
         <PreviewCard />
+        <button className="absolute top-1/2 right-0 hover:animate-bounce">
+          {/* //add a react spring animation instead of hover? */}
+          <img src={loadMore} alt="arrow to the right" className="w-14" />
+        </button>
       </div>
       {/*  //iterate over the list of audio books and add a previewcard for each show*/}
     </>

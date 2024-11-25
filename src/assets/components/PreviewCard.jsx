e4d524f9f2d470255a9aa00e828b35db979b7b9c
DaneVN <Dane.vN23@gmail.com>
2024-11-25 14:39:33 +0200

    Fetch + Display shows data from API

--- src/assets/components/PreviewCard.jsx
+++ src/assets/components/PreviewCard.jsx
@@ -1,44 +1,63 @@
 //eslint-disable-next-line
 import React from "react";
-import placeholder from "../images/placeholder-show-banner.jpg";
+import PropTypes from "prop-types";
 
-function PreviewCard() {
+function PreviewCard({ description, genres, image, seasons, title, updated }) {
   return (
     <div className="bg-lime-900 rounded-lg p-4 flex flex-col gap-4 sm:grid sm:grid-cols-3 sm:gap-4 mb-6">
       <div className=" mb-4">
         <img
           className="self-center rounded-lg"
-          src={placeholder}
+          src={image}
           alt="show banner/cover"
         />
       </div>
       <div className="col-span-2 flex flex-col items-center justify-evenly">
-        <h2 className="mb-4">Title</h2>
-        <p className="mb-2">
-          Description Description Description Description Description
-          Description Description Description{" "}
-          {/* //prettier added {" "}, for some reason? */}
-        </p>
+        <h2 className="mb-4">{title}</h2>
+        <p className="mb-2">{description}</p>
         <div className="flex gap-2 justify-center flex-wrap mb-2">
-          <p>Seasons</p>
-          <p>LastUpdated</p>
+          <p>{seasons}</p>
+          <p>{updated}</p>
         </div>
         <div id="genre-list">
           <ul className="list-none flex gap-2 justify-center flex-wrap">
-            <li id="genre" className="bg-slate-800 rounded-3xl px-3 py-1">
-              Genre
-            </li>
-            <li id="genre" className="bg-slate-800 rounded-3xl px-3 py-1">
-              Genre
-            </li>
-            <li id="genre" className="bg-slate-800 rounded-3xl px-3 py-1">
-              Genre
-            </li>
+            {genres.map((genre) => {
+              return (
+                <li
+                  id="genre"
+                  key={genre}
+                  className="bg-slate-800 rounded-3xl px-3 py-1"
+                >
+                  Genre
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
     </div>
   );
 }
+
+//Fixing ESlint bug on props destructuring: 'description/genre/etc' is missing in props validation
+// Define prop types for validation
+PreviewCard.propTypes = {
+  description: PropTypes.string,
+  genres: PropTypes.arrayOf(PropTypes.string),
+  image: PropTypes.string,
+  seasons: PropTypes.number,
+  title: PropTypes.string,
+  updated: PropTypes.string,
+};
+
+// Set default props to avoid undefined errors
+PreviewCard.defaultProps = {
+  description: "No description available.",
+  genres: [],
+  image: "",
+  seasons: 0,
+  title: "Untitled",
+  updated: "Unknown date",
+};
 
 export default PreviewCard;
--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,33 +1,55 @@
-//eslint-disable-next-line
 import React from "react";
 import LastListened from "../components/LastListened";
 import PreviewCard from "../components/PreviewCard";
 import { Link } from "react-router-dom";
 import loadMore from "../images/load-more.png";
 
-export default function Home() {
+function Home() {
+  // State to hold podcasts
+  const [podcasts, setPodcasts] = React.useState([]);
+
+  React.useEffect(() => {
+    // Fetch podcasts and sort them alphabetically by title
+    fetch("https://podcast-api.netlify.app")
+      .then((res) => res.json())
+      .then((data) => {
+        const sortedPodcasts = [...data].sort((a, b) =>
+          a.title.localeCompare(b.title)
+        );
+        setPodcasts(sortedPodcasts); // Set the sorted podcasts in state
+      })
+      .catch((err) => console.error("Error fetching podcasts:", err));
+  }, []);
+
   return (
     <>
       <h2>You were listening to:</h2>
       <LastListened />
       <br />
-      <div id="home-podcast-list" className="bg-lime-950 p-3 relative ">
-        {/* //have the different cards path to the corresponding show using urlParams */}
-        <Link to="show/1">
-          <PreviewCard />
-        </Link>
-        <Link to="show/2">
-          <PreviewCard />
-        </Link>
-        <Link to="show/3">
-          <PreviewCard />
-        </Link>
+      <div id="home-podcast-list" className="bg-lime-950 p-3 relative">
+        {/* Render sorted podcasts */}
+        {podcasts.map((show) => {
+          return (
+            <Link to={`show/${show.id}`} key={show.id}>
+              <PreviewCard
+                description={show.description}
+                genres={show.genres}
+                image={show.image}
+                seasons={show.seasons}
+                title={show.title}
+                updated={show.updated}
+              />
+            </Link>
+          );
+        })}
+
         <button className="absolute top-1/2 right-0 hover:animate-bounce">
-          {/* //add a react spring animation instead of hover? */}
+          {/* Add a react-spring animation here later */}
           <img src={loadMore} alt="arrow to the right" className="w-14" />
         </button>
       </div>
-      {/*  //iterate over the list of audio books and add a previewcard for each show*/}
     </>
   );
 }
+
+export default Home;

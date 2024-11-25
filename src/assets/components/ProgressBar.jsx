c327f35bc6350ce2f179fbf70db50a32c09202af
DaneVN <Dane.vN23@gmail.com>
2024-11-25 10:17:45 +0200

    show page + ProgressBar Responsiveness

--- src/assets/components/ProgressBar.jsx
+++ src/assets/components/ProgressBar.jsx
@@ -4,27 +4,36 @@ import PrevAudio from "../images/left.png";
 import Play from "../images/play-button.png";
 // import Pause from "../images/pause-button.png";
 import NextAudio from "../images/right.png";
+import starEmpty from "../images/star-empty.png";
 
 function ProgressBar() {
   // const [currentTrack, setCurrentTrack] = React.useState()
   return (
-    <section className="fixed bottom-0 flex justify-between w-full text-center pr-8 bg-slate-700">
-      <p>title</p>
-      <div id="controls" className="flex justify-center">
+    /* <section className="fixed bottom-0 flex justify-between items-center w-full h-16 text-center px-8 bg-slate-700"> */
+    <section className="fixed bottom-0 px-8 bg-slate-700 h-16 w-full items-center grid grid-cols-3 grid-rows-1 overflow-hidden">
+      <div className="items-center max-w-[90%] overflow-hidden sm:flex sm:gap-3">
+        <p className="truncate whitespace-nowrap text-ellipsis">
+          title title title title title title title
+        </p>
+        <button className="shrink-0 w-8">
+          <img src={starEmpty} alt="favourite" className="w-6" />
+        </button>
+      </div>
+      <div id="controls" className="flex justify-center gap-4">
         <button>
           <img className="w-8" src={PrevAudio} alt="previous audio" />
         </button>
         <button>
-          <img className="w-7" src={Play} alt="previous audio" />
+          <img className="w-7" src={Play} alt="play audio" />
         </button>
         {/* <button>
-          <img className="w-7" src={Pause} alt="previous audio" />
-        </button>   ---> conditional rendering based on if current track!*/}
+      <img className="w-7" src={Pause} alt="pause audio" />
+    </button>   ---> conditional rendering based on if current track is playing!*/}
         <button>
-          <img className="w-8" src={NextAudio} alt="previous audio" />
+          <img className="w-8" src={NextAudio} alt="next audio" />
         </button>
       </div>
-      <p>00:00</p>
+      <p className="justify-self-end">00:00</p>
     </section>
   );
 }
--- src/assets/pages/Show.jsx
+++ src/assets/pages/Show.jsx
@@ -2,23 +2,23 @@
 import React from "react";
 import backBtn from "../images/load-more.png";
 import starEmpty from "../images/star-empty.png";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
   return (
     <>
-      <section className=" bg-green-300 p-5 ">
+      <section className=" bg-green-300 p-5">
         <div
           id="show-details"
-          className="flex gap-7 justify-center items-start bg-green-300"
+          className="flex gap-7 justify-center items-start mb-4"
           //bg-image = season poster/ show poster when no season is selected
         >
-          <button>
-            <img src={backBtn} alt="Go back" className="rotate-180 w-12" />
+          <button className="shrink-0 w-8">
+            <img src={backBtn} alt="Go back" className="rotate-180" />
           </button>
           <div id="details" className="flex flex-col gap-3 mb-3">
             <h2>Title</h2>
-            <div className="flex gap-5 ">
+            <div className="flex gap-5 flex-wrap">
               {/* //load season pages based on amount of seasons in show */}
               {/* //add active style */}
               <NavLink to="" className="">
@@ -30,15 +30,6 @@ export default function Home() {
               <NavLink to="show.2" className="">
                 Season 3
               </NavLink>
-              <NavLink to="season" className="">
-                Season 4
-              </NavLink>
-              <NavLink to="season" className="">
-                Season 5
-              </NavLink>
-              <NavLink to="season" className="">
-                Season 6
-              </NavLink>
             </div>
             <p>
               Description Description Description Description Description
@@ -46,12 +37,27 @@ export default function Home() {
               Description{" "}
             </p>
           </div>
-          <button>
+          <button className="shrink-0 w-8">
             <img src={starEmpty} alt="favourite" className="w-12" />
           </button>
         </div>
         <div id="genre-list">
-          <ul className="list-none flex gap-2 justify-center flex-nowrap overflow-x-scroll">
+          <ul className="list-none flex gap-2 justify-start flex-nowrap overflow-x-scroll scrollbar">
+            <li id="genre" className="bg-slate-800 rounded-3xl px-3 py-1">
+              Genre
+            </li>
+            <li id="genre" className="bg-slate-800 rounded-3xl px-3 py-1">
+              Genre
+            </li>
+            <li id="genre" className="bg-slate-800 rounded-3xl px-3 py-1">
+              Genre
+            </li>
+            <li id="genre" className="bg-slate-800 rounded-3xl px-3 py-1">
+              Genre
+            </li>
+            <li id="genre" className="bg-slate-800 rounded-3xl px-3 py-1">
+              Genre
+            </li>
             <li id="genre" className="bg-slate-800 rounded-3xl px-3 py-1">
               Genre
             </li>
@@ -76,7 +82,6 @@ export default function Home() {
           </ul>
         </div>
       </section>
-      <Outlet />
     </>
   );
 }

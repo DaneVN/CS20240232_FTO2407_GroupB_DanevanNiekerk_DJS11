df419cd35d56a17aca4a59e07cafcc9841efdd61
DaneVN <Dane.vN23@gmail.com>
2024-11-27 11:20:45 +0200

    Card 'Loading...' Added

--- src/assets/components/PreviewCard.jsx
+++ src/assets/components/PreviewCard.jsx
@@ -46,58 +46,64 @@ function PreviewCard({
 
   return (
     <div className="bg-lime-900 rounded-lg p-4 mb-6 flex flex-col gap-4 md:col-span-1 lg:col-span-1 lg:grid lg:grid-cols-3 lg:gap-4">
-      <Link to={`show/${showId}`} key={showId}>
-        <div className="mb-4">
-          <img
-            className="self-center rounded-lg"
-            src={image}
-            alt="show banner/cover"
-          />
-        </div>
-      </Link>
-      <div className="col-span-2 flex flex-col items-center justify-evenly">
-        <Link to={`show/${showId}`}>
-          <h2 className="mb-4">{title}</h2>
-        </Link>
-        <div className="mb-2 relative">
-          <p
-            ref={descriptionRef}
-            className={`transition-all ${
-              isExpanded ? "line-clamp-none" : "line-clamp-3"
-            }`}
-          >
-            {description}
-          </p>
-          {/* Toggle Button */}
-          {isOverflowing && (
-            <button
-              onClick={() => setIsExpanded((prev) => !prev)}
-              className="text-blue-500 underline mt-2"
-            >
-              {isExpanded ? "Show Less" : "Show More"}
-            </button>
-          )}
-        </div>
-        <div className="flex gap-2 justify-center flex-wrap mb-2">
-          <p>{seasons ? `${seasons} Seasons` : "No seasons available"}</p>
-          <p>|</p>
-          <p>Updated: {updatedParsed}</p>
-        </div>
-        <div id="genre-list">
-          <ul className="list-none flex gap-2 justify-center flex-wrap">
-            {Array.isArray(genres) &&
-              genres.map((genreNum) => (
-                <li
-                  id="genre"
-                  key={genreNum}
-                  className="bg-slate-800 rounded-3xl px-3 py-1"
+      {image ? (
+        <>
+          <Link to={`show/${showId}`} key={showId}>
+            <div className="mb-4">
+              <img
+                className="self-center rounded-lg"
+                src={image}
+                alt="show banner/cover"
+              />
+            </div>
+          </Link>
+          <div className="col-span-2 flex flex-col items-center justify-evenly">
+            <Link to={`show/${showId}`}>
+              <h2 className="mb-4">{title}</h2>
+            </Link>
+            <div className="mb-2 relative">
+              <p
+                ref={descriptionRef}
+                className={`transition-all ${
+                  isExpanded ? "line-clamp-none" : "line-clamp-3"
+                }`}
+              >
+                {description}
+              </p>
+              {/* Toggle Button */}
+              {isOverflowing && (
+                <button
+                  onClick={() => setIsExpanded((prev) => !prev)}
+                  className="text-blue-500 underline mt-2"
                 >
-                  {genresStrings[genreNum - 1][1]}
-                </li>
-              ))}
-          </ul>
-        </div>
-      </div>
+                  {isExpanded ? "Show Less" : "Show More"}
+                </button>
+              )}
+            </div>
+            <div className="flex gap-2 justify-center flex-wrap mb-2">
+              <p>{seasons ? `${seasons} Seasons` : "No seasons available"}</p>
+              <p>|</p>
+              <p>Updated: {updatedParsed}</p>
+            </div>
+            <div id="genre-list">
+              <ul className="list-none flex gap-2 justify-center flex-wrap">
+                {Array.isArray(genres) &&
+                  genres.map((genreNum) => (
+                    <li
+                      id="genre"
+                      key={genreNum}
+                      className="bg-slate-800 rounded-3xl px-3 py-1"
+                    >
+                      {genresStrings[genreNum - 1][1]}
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          </div>
+        </>
+      ) : (
+        <h3>Loading preview...</h3>
+      )}
     </div>
   );
 }

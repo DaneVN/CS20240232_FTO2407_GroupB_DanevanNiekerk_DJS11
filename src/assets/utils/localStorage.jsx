6dcc40bbeb94def2c78cd347bcb53869f081f744
DaneVN <Dane.vN23@gmail.com>
2024-11-27 06:19:00 +0200

    Favourites Utilities refactor

--- src/assets/utils/localStorage.jsx
+++ src/assets/utils/localStorage.jsx
@@ -1,40 +1,30 @@
-// get/set favourites in local storage
-export let favourites = [];
+export const favourites = {
+  episodes: [localStorage.getItem("favouriteEpisodes")],
 
-export const getFavouritesArray = () => {
-  try {
-    favourites.push(
-      ...(localStorage.getItem("favouriteEpisodes") ||
-        "None yet! Click the starIcon to add a Favourite!")
-    );
-  } catch (err) {
-    console.log("Failed to retrieve episodes from Local storage: ", err);
-  }
-};
+  addFavouritesToLS: (episodeString) => {
+    try {
+      this.episodes = [...this.episodes, episodeString];
+      localStorage.setitem("favouriteEpisodes", ...this.episodes);
+    } catch (err) {
+      console.log("Failed to add episode to favourites in LocalStorage: ", err);
+    }
+  },
 
-export const checkFavourites = (episodeString) => {
-  const favouritesArray = getFavouritesArray();
-  favouritesArray.filter((item) => {
-    item === episodeString;
-  });
-};
+  removeFromLS: (episodeString) => {
+    try {
+      this.episodes = [...this.episodes].filter((item) => {
+        item !== episodeString;
+      });
+      localStorage.setitem("favouriteEpisodes", ...this.episodes);
+    } catch (err) {
+      console.log("Failed to add episode to favourites in LocalStorage: ", err);
+    }
+  },
 
-export const removeFromLS = (episodeString) => {
-  try {
-    favourites = [...favourites].filter((item) => {
-      item !== episodeString;
+  checkFavourites: (episodeString) => {
+    const favouritesArray = this.episodes;
+    favouritesArray.filter((item) => {
+      item === episodeString;
     });
-    localStorage.setitem("favouriteEpisodes", ...favourites);
-  } catch (err) {
-    console.log("Failed to add episode to favourites in LocalStorage: ", err);
-  }
-};
-
-export const addFavouriteToLS = (episodeString) => {
-  try {
-    favourites = [...favourites, episodeString];
-    localStorage.setitem("favouriteEpisodes", ...favourites);
-  } catch (err) {
-    console.log("Failed to add episode to favourites in LocalStorage: ", err);
-  }
+  },
 };
